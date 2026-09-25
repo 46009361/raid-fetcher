4716527194987dc61a85fa0a42174f61c9b11b79
46009361 <me@46009361.com>
2026-09-24 21:28:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const ALLOWED_HOST = 'share.redd.it';
 
 app.use(cors({
-  origin: 'null',        // only this origin allowed
+  origin: `https://${ALLOWED_HOST}`,        // only this origin allowed
   methods: ['GET'],                    // only these methods
   allowedHeaders: ['Content-Type'],    // only these request headers
 }));
